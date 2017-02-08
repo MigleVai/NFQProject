e621d60634d132b24750f3d6fa87c96c188e7c55
Potatano <migle.vaituleviciute@gmail.com>
2017-02-08 19:29:17 +0200

    Database creation scripts
    Created database "books" and table "book"

---
+++ CreateTable.sql
@@ -0,0 +1,8 @@
+Create Table Book(
+Title varchar(20) not null,
+ReleaseDate DateTime not null,
+Author varchar(50) not null,
+Genre varchar(20),
+Id int not null auto_increment,
+primary key(Id)
+);
